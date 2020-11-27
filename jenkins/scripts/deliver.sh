e574b281b15a7c93a08859ee983b384fb8733197
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-11-27 19:14:33 +0500

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,7 +6,9 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm run build
+ssh ubuntu@3.86.0.129
+mkdir test_bahi
+//npm run build
 set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
@@ -17,11 +19,11 @@ echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
-set -x
-npm start &
-sleep 1
-echo $! > .pidfile
-set +x
+//set -x
+//npm start &
+//sleep 1
+//echo $! > .pidfile
+//set +x
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
